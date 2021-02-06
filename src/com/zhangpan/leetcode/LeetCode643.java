93bc1d0ef4ad2d4f825121691e42a5141296d9c5
zhpanvip <zhpanvip@outlook.com>
2021-02-06 23:28:53 +0800

    LeetCode001 and LeetCode643.

---
+++ src/com/zhangpan/leetcode/LeetCode643.java
@@ -0,0 +1,39 @@
+package com.zhangpan.leetcode;
+
+/**
+ * 给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。
+ *
+ * 示例：
+ *
+ * 输入：[1,12,-5,-6,50,3], k = 4
+ * 输出：12.75
+ * 解释：最大平均数 (12-5-6+50)/4 = 51/4 = 12.75
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= k <= n <= 30,000。
+ * 所给数据范围 [-10,000，10,000]。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-average-subarray-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode643 {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        if(nums.length==k){
+            return (double)sum/k;
+        }
+        int maxSum=sum;
+        for(int i=0;i<nums.length-k;i++){
+            sum=sum-nums[i]+nums[i+k];
+            maxSum=Math.max(maxSum,sum);
+        }
+        return (double)maxSum/k;
+
+    }
+}
