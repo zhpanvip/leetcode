794d7ced0fb75cea3a1f608812f730fe6202ab64
zhpanvip <zhpanvip@outlook.com>
2021-02-27 01:32:07 +0800

    LeetCode206 反转链表

---
+++ src/com/zhangpan/leetcode/LeetCode092.java
@@ -0,0 +1,24 @@
+package com.zhangpan.leetcode;
+
+/**
+ * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+ * <p>
+ * 说明:
+ * 1 ≤ m ≤ n ≤ 链表长度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode092 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        return head;
+    }
+}
