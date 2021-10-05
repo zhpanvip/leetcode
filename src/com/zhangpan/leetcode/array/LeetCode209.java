f8b7bea53ca08e3afe2c1aa6a2fe9fe42ca7e902
zhpanvip <zhpanvip@outlook.com>
2021-10-05 09:35:39 +0800

    leetcode 209

---
+++ src/com/zhangpan/leetcode/array/LeetCode209.java
@@ -0,0 +1,52 @@
+package com.zhangpan.leetcode.array;
+
+/**
+ * 给定一个含有n个正整数的数组和一个正整数 target 。
+ *
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组[4,3]是该条件下的长度最小的子数组。
+ * 示例 2：
+ *
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *
+ * 提示：
+ *
+ * 1 <= target <= 109
+ * 1 <= nums.length <= 105
+ * 1 <= nums[i] <= 105
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        int res = Integer.MAX_VALUE;
+        int left = 0,right = 0;
+        int sum = 0;
+        while(right < n){
+            sum += nums[right];
+            while(sum >= target && left <= right){
+                res = Math.min(res,right - left +1);
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return res == Integer.MAX_VALUE ? 0: res;
+    }
+}
--- src/com/zhangpan/leetcode/array/LeetCode53.java
+++ src/com/zhangpan/leetcode/array/LeetCode53.java
@@ -1,5 +1,33 @@
 package com.zhangpan.leetcode.array;
 
+/**
+ * 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：0
+ * 示例 4：
+ *
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 示例 5：
+ *
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ *
+ * https://leetcode-cn.com/problems/maximum-subarray/
+ */
 public class LeetCode53 {
     public int maxSubArray(int[] nums) {
         int res = nums[0];
